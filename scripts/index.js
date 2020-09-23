0e9125d580112f38792c1647bf24c08c020be6a6
Brendon Jennett <vxp101@gmail.com>
2020-09-22 21:35:09 -0400

    This is not an update

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
+import { render } from "./ui.js";
 // file handles events
 //submission of bookmark
 // editing of bookmark
 // delete event
 // listing bookmark
 // all of the even handlers and listenrs go here
-import { render } from "./ui.js";
+
 
 
 $(render())
-
-
